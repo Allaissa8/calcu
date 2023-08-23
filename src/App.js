f03e33102f5f960854a4784ae82cc4f52e3653e7
Al <calizolai@gmail.com>
2023-08-24 01:52:58 +0800

    final na siguro - with functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Button from './components/buttons';
+import NumberInput from './components/input';
+import { useState } from 'react';
 function App() {
 
-  const [first, setFirst ] = useState('');
-  const [second, setSecond ] = useState('');
+  const [first, setFirst] = useState('');
+  const [second, setSecond] = useState('');
   const [result, setResult] = useState('');
 
   const handleFirstChange = (e) => {
     const value = e.target.value;
     if (/^[0-9]*$/.test(value)) {  //check kung positive or negative yung input
       setFirst(value);
-  }
+    }
   };
 
   const handleSecondChange = (e) => {
@@ -22,29 +23,42 @@ function App() {
   };
 
   const handleOperations = (operation) => {
-    if (operation  == "addition"){
+    if (operation === 'addition') {
       setResult(parseFloat(first) + parseFloat(second));
     }
-    else if (operation  == "subtraction"){
+    else if (operation === 'subtraction') {
       setResult(parseFloat(first) - parseFloat(second));
     }
-    else if (operation  == "multiply"){
+    else if (operation === 'multiply') {
       setResult(parseFloat(first) * parseFloat(second));
     }
-    else if (operation  == "divide"){
+    else if (operation === 'divide') {
       setResult(parseFloat(first) / parseFloat(second));
     }
-    else{
+    else {
       setResult('');
     }
-      
-    }
   };
+
 
   return (
     <>
       <div className="App">
+        <div className="TitleBg">
+          <h1>Allaisa Soriano's Activity</h1>
+        </div>
+        <div className ="Buttons"> 
+          <NumberInput value={first} onChange={handleFirstChange} placeholder="First number" />
+          <NumberInput value={second} onChange={handleSecondChange} placeholder="Second number" />
+        </div>
+        <div className="Operations">
+          <Button onClick={() => handleOperations('addition')}>Add</Button>
+          <Button onClick={() => handleOperations('subtraction')}>Subtract</Button>
+          <Button onClick={() => handleOperations('multiply')}>Multiply</Button>
+          <Button onClick={() => handleOperations('divide')}>Divide</Button>
 
+        </div>
+        <h1>Answer is: {result}</h1>
       </div>
     </>
   );
